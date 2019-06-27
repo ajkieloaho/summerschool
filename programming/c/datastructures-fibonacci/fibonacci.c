dded628abbe1dc5d6463b80824c8708d52514ae1
Antti-Jussi Kieloaho <antti-jussi.kieloaho@luke.fi>
2019-06-27 15:28:43 +0300

    last excersice before hell

--- programming/c/datastructures-fibonacci/fibonacci.c
+++ programming/c/datastructures-fibonacci/fibonacci.c
@@ -7,6 +7,22 @@ int main(int argc, char *argv[])
     int i;
 
     // initialize necessary array elements and write the for loop
+	int f0, f1, f2;
+
+	f0 = 0;
+	f1 = 1;
+
+	f[0] = f0;
+	f[1] = f1;
+
+	for (i = 2; i < 20; i++) {
+		f2 = f1 + f0;
+
+		f0 = f1;
+		f1 = f2;
+		f[i] = f2;
+	}
+
 
     printf("First 20 Fibonacci numbers are:\n");
     for (i = 0; i < 20; i++) {
