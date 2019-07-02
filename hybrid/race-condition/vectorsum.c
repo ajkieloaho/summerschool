afc4d5e1912c01af626bb2fa3c3d70d4e1f6207c
ajkieloaho <antti-jussi.kieloaho@luke.fi>
2019-07-02 10:39:08 +0300

    race condition, reduction and critical excercises

--- hybrid/race-condition/vectorsum.c
+++ hybrid/race-condition/vectorsum.c
@@ -19,10 +19,46 @@ int main(void)
 
     sum = 0.0;
     /* TODO: Parallelize computation */
+
+    #pragma omp parallel for default(shared) private(i)
     for (i = 0; i < NX; i++) {
         sum += vecA[i];
     }
-    printf("Sum: %ld\n", sum);
+    printf("Sum with data race: %ld\n", sum);
+    
+    sum = 0.0;
+    #pragma omp parallel for default(shared) private(i)   
+    for (i = 0; i < NX; i++) {
+	#pragma omp critical(dummy)
+	sum += vecA[i];
+    }
+
+    printf("Sum using critical section: %ld\n", sum);
+
+
+    sum = 0.0;
+    #pragma omp parallel default(shared) private(i, psum)
+{
+    psum = 0.0;
+
+    #pragma omp for
+    for (i = 0; i < NX; i++) {
+	psum += vecA[i];
+    }
+
+    #pragma omp critical(par_sum)
+    sum += psum;
+}
+    printf("Sum using private variable and critical section: %ld\n", sum);
+
+
+    sum = 0.0;
+    #pragma omp parallel for default(shared) private(i) reduction(+:sum)
+    for (i = 0; i < NX; i++) {
+	sum += vecA[i];
+   }
+    printf("Reduction sum: %ld\n", sum);
+
 
     return 0;
 }
